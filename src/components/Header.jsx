465b153b1c5514c5905f22a87d2b96f2d9c5f9a0
Suvradippaul <suvradip2000@gmail.com>
2022-10-29 22:44:55 +0530

    feat: Add responsive layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import "../assets/Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import Icon from '../assets/images/icon.png';
 
 const Header = ({ handleSearch }) => {
   return (
     <div className="header">
-      <div className="logo-area">Keep Notes 📑</div>
+      {/* <div className="logo-area">Keep Notes 📑</div> */}
+      <div className="logo-area">
+        Keep Notes 
+        <img src={Icon} style={{"width": "30px", "height": "30px"}} id="icon"/>
+        </div>
       <div className="search-area">
         <SearchIcon id="search-icon"></SearchIcon>
         <input
@@ -12,6 +17,9 @@ const Header = ({ handleSearch }) => {
           name=""
           id="search"
           placeholder="Search notes"
+          autoComplete="off"
+          autoCapitalize="off"
+          autoCorrect="off"
           onChange={(e) => handleSearch(e.target.value)}
         />
       </div>
